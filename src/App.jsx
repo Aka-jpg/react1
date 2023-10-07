8451f4b73bc2c4ee893f8e5da754dfa558075eca
Akash Nepal <akashnepal72@gmail.com>
2023-10-07 11:39:09 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import picture from './contact.jpg' 
+import './App.css'
+
+function App() {
+  return (
+    <>
+       <nav>
+<img src={picture}  width='120'></img>
+        <h2>Akash Nepal</h2>
+         <h4> Curriculum Vitae</h4>
+        </nav>
+        
+     <main>
+<h1> My Portfolio</h1>
+   <h2> Short Introduction</h2>
+  <ul>
+    <li> Final Year Student </li>
+    <li> Knowledge of 3 programmingf languages</li>
+    <li> Seeking an opportunity for internship</li>
+    </ul>
+<h2> Hobbies </h2>
+<ol>
+  <li> Coding </li>
+  <li> Watching Movies </li>
+  <li> Traveling</li>
+  </ol>
+<h2> Novels</h2>
+<ol>
+  <li>Harry Potter </li>
+  <li> China harayeko manche</li>
+</ol>
+  
+    </main>
+      
+
+      
+        
+      
+    </>
+  )
+}
+
+export default App
